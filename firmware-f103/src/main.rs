fcaf923053822a49f7ad7df40a60a69990dc097d
Kevin J. Lynagh <kevin@keminglabs.com>
2020-06-06 21:49:47 +0800

    Interrupt to toggle led working.

--- firmware-f103/src/main.rs
+++ firmware-f103/src/main.rs
@@ -1,14 +1,11 @@
 #![no_std]
 #![no_main]
 
-//CDC-ACM serial port example using polling in a busy loop.
-//copied from https://github.com/stm32-rs/stm32-usbd-examples/blob/master/example-stm32f072rb/src/main.rs
-
 extern crate panic_semihosting;
 
 mod counter;
 
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::v2::{InputPin, OutputPin};
 use stm32_usbd::UsbBus;
 use stm32f1xx_hal::stm32 as hw;
 use stm32f1xx_hal::{gpio, prelude::*, usb, usb::UsbBusType};
@@ -24,6 +21,7 @@ const APP: () = {
         midi: usbd_midi::midi_device::MidiClass<'static, UsbBusType>,
         usb_device: UsbDevice<'static, UsbBusType>,
         led: gpio::gpioc::PC13<gpio::Output<gpio::PushPull>>,
+        input: gpio::gpiob::PB12<gpio::Input<gpio::PullUp>>,
     }
 
     // Interrupt handlers used to dispatch software tasks
@@ -32,8 +30,8 @@ const APP: () = {
     }
 
     #[init]
-    fn init(mut cx: init::Context) -> init::LateResources {
-        let mut dp = hw::Peripherals::take().unwrap();
+    fn init(_cx: init::Context) -> init::LateResources {
+        let dp = hw::Peripherals::take().unwrap();
 
         //Configure button interrupt
         // Enable the alternate function I/O clock (for external interrupts)
@@ -59,20 +57,21 @@ const APP: () = {
         ////////////////
         //Configure button interrupt
         let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
-        gpiob.pb11.into_pull_up_input(&mut gpiob.crh);
+        let input = gpiob.pb12.into_pull_up_input(&mut gpiob.crh);
 
-        // Set EXTI11 multiplexers to use port B
-        dp.AFIO.exticr3.write(|w| unsafe { w.exti11().bits(0x01) });
-        // Enable interrupt on EXTI11
-        dp.EXTI.imr.write(|w| w.mr11().set_bit());
-        // Set falling trigger selection for EXTI11
-        dp.EXTI.ftsr.write(|w| w.tr11().set_bit());
+        // Set EXTI12 multiplexers to use port B
+        dp.AFIO.exticr4.write(|w| unsafe { w.exti12().bits(0x01) });
+        // Enable interrupt on EXTI12
+        dp.EXTI.imr.write(|w| w.mr12().set_bit());
+        // Set falling and rising trigger selection for EXTI12
+        dp.EXTI.ftsr.write(|w| w.tr12().set_bit());
+        dp.EXTI.rtsr.write(|w| w.tr12().set_bit());
 
         ////////////////
         //Configure USB
         static mut USB_BUS: Option<usb_device::bus::UsbBusAllocator<UsbBusType>> = None;
 
-        let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+        let gpioa = dp.GPIOA.split(&mut rcc.apb2);
         unsafe {
             USB_BUS = Some(UsbBus::new(usb::Peripheral {
                 usb: dp.USB,
@@ -84,10 +83,10 @@ const APP: () = {
             let usb_bus = USB_BUS.as_ref().unwrap();
 
             let max_packet_size = 16;
-            let mut counter = counter::Counter::new(usb_bus, max_packet_size);
-            let mut midi = usbd_midi::midi_device::MidiClass::new(usb_bus);
+            let counter = counter::Counter::new(usb_bus, max_packet_size);
+            let midi = usbd_midi::midi_device::MidiClass::new(usb_bus);
 
-            let mut usb_device = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x16c0, 0x27dd))
+            let usb_device = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x16c0, 0x27dd))
                 .manufacturer("Fake company")
                 .product("Foo")
                 .serial_number("TEST")
@@ -97,10 +96,11 @@ const APP: () = {
 
             init::LateResources {
                 exti: dp.EXTI,
-                usb_device: usb_device,
-                counter: counter,
-                midi: midi,
-                led: led,
+                usb_device,
+                counter,
+                midi,
+                led,
+                input,
             }
         }
     }
@@ -118,17 +118,16 @@ const APP: () = {
         }
     }
 
-    #[task(priority = 2, binds = EXTI15_10, resources = [exti, led])]
+    #[task(priority = 2, binds = EXTI15_10, resources = [exti, led, input])]
     fn exti15_10(mut c: exti15_10::Context) {
         //clear interrupt
-        c.resources.exti.pr.modify(|_, w| w.pr11().set_bit());
+        c.resources.exti.pr.modify(|_, w| w.pr12().set_bit());
 
         let led = &mut c.resources.led;
-
-        if led.is_set_high().unwrap() {
-            led.set_low().unwrap();
-        } else {
+        if c.resources.input.is_high().unwrap() {
             led.set_high().unwrap();
+        } else {
+            led.set_low().unwrap();
         }
     }
 };
